e9132ff9df8dbfe145cabf3af5a5f8043f9cebde
unknown <amengede27@outlook.com>
2022-06-25 09:59:33 +1000

    SPIR-V Files

---
+++ pyvulkan/10 - SPIR-V Files/finished/frame.py
@@ -0,0 +1,9 @@
+from config import *
+
+class SwapChainFrame:
+
+
+    def __init__(self):
+        
+        self.image = None
+        self.image_view = None
---
+++ vulkan/10 - SPIR-V files/Finished/shaders/compile.bat
@@ -0,0 +1,2 @@
+C:\VulkanSDK\1.2.198.1\Bin\glslc.exe shader.vert -o vertex.spv
+C:\VulkanSDK\1.2.198.1\Bin\glslc.exe shader.frag -o fragment.spv
