58f081db8d8eeb9fe9f96c097aa8c37cb1481819
unknown <amengede27@outlook.com>
2022-08-05 15:08:00 +1000

    Multithreaded Rendering: Python

---
+++ pyvulkan/13 - Multithreaded Rendering/start/frame.py
@@ -0,0 +1,11 @@
+from config import *
+
+class SwapChainFrame:
+
+
+    def __init__(self):
+        
+        self.image = None
+        self.image_view = None
+        self.framebuffer = None
+        self.commandbuffer = None
