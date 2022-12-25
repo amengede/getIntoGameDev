9bbbd3b12028ed692e8b978a8283e11bb6ef64a6
Andrew Mengede <amengede27@outlook.com>
2022-12-25 22:11:44 +1000

    Added Visual Studio Project Files: 07

--- vulkan/07 - Window Surface Creation/StartPoint/logging.h
+++ vulkan/07 - Window Surface Creation/StartPoint/logging.h
@@ -37,6 +37,16 @@ namespace vkInit {
 		} VkDebugUtilsMessengerCallbackDataEXT;
 
 	*/
+
+	/**
+		Logging callback function.
+
+		\param messageSeverity describes the severity level of the message
+		\param messageType describes the type of the message
+		\param pCallbackData standard data associated with the message
+		\param pUserData custom extra data which can be associated with the message
+		\returns whether to end program execution
+	*/
 	VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
 		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
 		VkDebugUtilsMessageTypeFlagsEXT messageType,
@@ -48,6 +58,13 @@ namespace vkInit {
 		return VK_FALSE;
 	}
 
+	/**
+		Make a debug messenger
+
+		\param instance The Vulkan instance which will be debugged.
+		\param dldi dynamically loads instance based dispatch functions
+		\returns the created messenger
+	*/
 	vk::DebugUtilsMessengerEXT make_debug_messenger(vk::Instance& instance, vk::DispatchLoaderDynamic& dldi) {
 
 		/*
