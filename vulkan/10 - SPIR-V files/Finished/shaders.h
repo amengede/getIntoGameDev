09c819558b6b34ee47cb329e0f301b4e513e2b6b
Andrew Mengede <amengede27@outlook.com>
2022-12-26 00:16:00 +1000

    Added Visual Studio Project Files: 10

--- vulkan/10 - SPIR-V files/Finished/shaders.h
+++ vulkan/10 - SPIR-V files/Finished/shaders.h
@@ -3,6 +3,13 @@
 
 namespace vkUtil {
 
+	/**
+		Read a file.
+
+		\param filename a string representing the path to the file
+		\param debug whether the system is running in debug mode
+		\returns the contents as a vector of raw binary characters
+	*/
 	std::vector<char> readFile(std::string filename, bool debug) {
 		
 		std::ifstream file(filename, std::ios::ate | std::ios::binary);
