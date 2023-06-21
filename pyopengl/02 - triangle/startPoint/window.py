75698b85d25a6c2724681835a8839728b248287d
Andrew Mengede <amengede27@outlook.com>
2023-06-21 19:27:53 +1000

    2023 update: pyOpenGL Triangle

--- pyopengl/02 - triangle/startPoint/window.py
+++ pyopengl/02 - triangle/startPoint/window.py
@@ -2,23 +2,48 @@ import pygame as pg
 from OpenGL.GL import *
 
 class App:
+    """
+        For now, the app will be handling everything.
+        Later on we'll break it into subcomponents.
+    """
 
 
     def __init__(self):
         """ Initialise the program """
-        #initialise pygame
+        
+        self._set_up_pygame()
+        
+        self._set_up_timer()
+
+        self._set_up_opengl()
+    
+    def _set_up_pygame(self) -> None:
+        """
+            Initialize and configure pygame.
+        """
+
         pg.init()
         pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
         pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
         pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK,
                                     pg.GL_CONTEXT_PROFILE_CORE)
         pg.display.set_mode((640,480), pg.OPENGL|pg.DOUBLEBUF)
-        self.clock = pg.time.Clock()
-        #initialise opengl
-        glClearColor(0.1, 0.2, 0.2, 1)
-        self.mainLoop()
+    
+    def _set_up_timer(self) -> None:
+        """
+            Set up the app's timer.
+        """
 
-    def mainLoop(self):
+        self.clock = pg.time.Clock()
+
+    def _set_up_opengl(self) -> None:
+        """
+            Configure any desired OpenGL options
+        """
+
+        glClearColor(0.1, 0.2, 0.2, 1)
+
+    def run(self) -> None:
         """ Run the app """
 
         running = True
@@ -33,11 +58,12 @@ class App:
 
             #timing
             self.clock.tick(60)
-        self.quit()
 
-    def quit(self):
+    def quit(self) -> None:
         """ cleanup the app, run exit code """
         pg.quit()
 
 if __name__ == "__main__":
     myApp = App()
+    myApp.run()
+    myApp.quit()
